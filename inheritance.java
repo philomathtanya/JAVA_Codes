d45b8b86454bccec4ecaf29b09e9a1cf89c0f7eb
philomathtanya <tanyabansal525@gmail.com>
2021-10-09 21:33:01 +0530

    inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,17 @@
+public class inheritance{
+    void eat(){System.out.println("eating...");}
+}
+class Dog extends inheritance{
+    void bark(){System.out.println("barking...");}
+}
+class BabyDog extends Dog{
+    void weep(){System.out.println("weeping...");}
+}
+class TestInheritance2{
+    public static void main(String args[]){
+        BabyDog d=new BabyDog();
+        d.weep();
+        d.bark();
+        d.eat();
+    }}
+
