8232e0e89e9412785fe5dbb9524e953072aa12c2
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-11-16 09:21:59 +0530

    Conversion_ass2

---
+++ Conversion_ass2.java
@@ -0,0 +1,9 @@
+
+
+public class Conversion_ass2 {
+    public static void main(String[] args) {
+        int n=Integer.parseInt(args[0]);
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(Integer.toHexString(n));
+        System.out.println(Integer.toOctalString(n));
+    }}
