a0dd3b1884eb1064c1c67fa6f8e17096beb0f1a2
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2021-11-24 22:28:21 +0530

    colours

---
+++ colours.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class colours implements Runnable {
+    public static void main(String[] args) {
+        colours obj=new colours();
+        Thread t1=new Thread(obj);
+        t1.start();
+    }
+    @Override
+    public void run() {
+        Random random=new Random();
+        String colours[]={"white","blue","black","green","red","yellow"};
+        int index;
+        while((index=random.nextInt(6))!=4)
+        {
+            System.out.println(colours[index]);
+        }
+    }
+}
